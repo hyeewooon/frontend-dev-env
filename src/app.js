1ed4c3a93daa1e7202de9cb17ac286d132e915db
hyewon <hyeewooon@naver.com>
2022-08-12 10:09:18 +0900

    feat: 커스텀 로더 설정 및 스타일 시트 모듈 처리

--- src/app.js
+++ src/app.js
@@ -1,7 +1,4 @@
 import * as math from "./math.js";
-import { scrollHandler } from "./intersectionObserver.js";
+import "./app.css";
 
 console.log(math.sum(1, 2));
-
-const boxElem = document.querySelectorAll(".box");
-scrollHandler(boxElem);
--- webpack.config.js
+++ webpack.config.js
@@ -9,4 +9,16 @@ module.exports = {
     filename: "[name].js",
     path: path.resolve("./dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [path.resolve("./my-webpack-loader.js")],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
